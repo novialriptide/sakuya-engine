0033b945af7768353091dca99c738013580dd77c
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-11-23 12:00:18 -0500

    made screen to be resizable

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import pygame
-from .errors import NoActiveScene
+
+from copy import copy
+
+from .errors import NoActiveSceneError, NotImplementedError
 from .math import Vector
 
 class Client:
@@ -7,7 +10,9 @@ class Client:
         self,
         window_name: str,
         window_size: Vector,
-        window_icon: pygame.Surface = None
+        window_icon: pygame.Surface = None,
+        resizeable_window: bool = True,
+        keep_aspect_ratio: bool = True
     ) -> None:
         """
         The game's main client
@@ -17,15 +22,22 @@ class Client:
             window_size: the window size
         """
         self.is_running = True
-        self._window_name = window_name
-        self.window_size = window_size
+        self._window_name = window_name # str
+        self.original_window_size = window_size # Vector
         self.window_icon = window_icon
+        self.keep_aspect_ratio = keep_aspect_ratio # bool
+        self.original_aspect_ratio = window_size.ratio_xy
         self.running_scenes = {}
         self.scene_manager = None
         self.pg_clock = pygame.time.Clock()
 
+        self.pg_flag = 0
+        if resizeable_window:
+            self.pg_flag = pygame.RESIZABLE
+
         self.screen = pygame.display.set_mode(
-            (self.window_size.x, self.window_size.y)
+            (window_size.x, window_size.y),
+            self.pg_flag
         )
         pygame.display.set_caption(self._window_name)
 
@@ -44,13 +56,36 @@ class Client:
         self._window_name = value
         pygame.display.set_caption(self._window_name)
 
+    @property
+    def window_size(self) -> Vector:
+        screen_rect = self.screen.get_rect()
+        return Vector(screen_rect.x, screen_rect.y)
+
+    @property
+    def adjust_to_screensize(self) -> Vector:
+        raise NotImplementedError
+        
+        return Vector()
+
     def main(self) -> None:
         """
         Main game loop
         """
         while(self.is_running):
             if self.running_scenes == []:
-                raise NoActiveScene
+                raise NoActiveSceneError
+
+            if self.keep_aspect_ratio:
+                pg_event = pygame.event.get(
+                    eventtype=pygame.VIDEORESIZE,
+                    pump=True
+                )
+                if pg_event != []:
+                    self.screen = pygame.display.set_mode((
+                        pg_event[0].w, 
+                        pg_event[0].w * self.original_window_size.ratio_yx),
+                        self.pg_flag
+                    )
 
             for s in self.running_scenes:
                 s = self.running_scenes[s]["scene"]
