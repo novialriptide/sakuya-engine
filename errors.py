42e20a23af6793a264ec0bbf63fba1dc8ef04bc1
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-11-23 11:58:47 -0500

    fixed error exception names

--- errors.py
+++ errors.py
@@ -1,14 +1,18 @@
 class Error(Exception):
     pass
 
+class NotImplementedError(Error):
+    def __init__(self):
+        self.message = "This feature is not available"
+
 class NegativeSpeedError(Error):
     def __init__(self):
         self.message = "Speed cannot be negative"
 
-class ObjectNotInWorld(Error):
+class ObjectNotInWorldError(Error):
     def __init__(self):
         self.message = "Object is not in the world"
 
-class NoActiveScene(Error):
+class NoActiveSceneError(Error):
     def __init__(self):
         self.message = "No scene is active"
