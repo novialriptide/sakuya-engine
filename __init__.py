fd7900a7ab00d46f17c9ba34b405354ae4819fd5
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-11-25 15:29:12 -0500

    version bump

--- __init__.py
+++ __init__.py
@@ -13,5 +13,5 @@ from .text import *
 from .tile import *
 from .time import *
 
-__version__ = "2.0"
+__version__ = "2.0.1"
 print(f"running sakuya {__version__} by novial (using pygame {pg_ver})\nsource code: https://github.com/novialriptide/Sakuya")
