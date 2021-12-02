ce180694a5cf7c0802937c4563bb9a6e1ed3dbfc
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-12-02 10:35:45 -0500

    version bump

--- __init__.py
+++ __init__.py
@@ -16,5 +16,5 @@ from .text import *
 from .tile import *
 from .waves import *
 
-__version__ = "2.0.1"
+__version__ = "2.2.1"
 print(f"sakuya engine {__version__} by novial (using pygame {pg_ver})\nsource code: https://github.com/novialriptide/Sakuya")
