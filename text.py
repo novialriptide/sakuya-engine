0597742247b50fe83dc138f6ca7d41bc64b2944f
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-03 18:32:31 -0500

    Update text.py

--- text.py
+++ text.py
@@ -1,23 +1,21 @@
 """
 SakuyaEngine // GameDen (c) 2020-2021 Andrew Hong
 This code is licensed under MIT license (see LICENSE for details)
-"""
-import pygame
-pygame.font.init()
 
-"""
-v1.0
 This source code is taken straight
 from GameDen Engine for Novial's Gravity.
 Therefore, parameters should be the same.
 """
+import pygame
 
 def text(text: str, size: int, sys_font: str, color: str) -> pygame.Surface:
+    pygame.font.init()
     formatting = pygame.font.SysFont(sys_font,int(size))
     text_surface = formatting.render(text,True,color)
     return text_surface
 
 def text2(text: str, size: int, font: str, color: str) -> pygame.Surface:
+    pygame.font.init()
     formatting = pygame.font.Font(font,int(size))
     text_surface = formatting.render(text,True,color)
     return text_surface
