40224dd40b85b93d00d4816ae5fafe04efd2c1a8
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-05 01:10:44 -0500

    added proper pep8 formatting

--- bullets.py
+++ bullets.py
@@ -3,28 +3,52 @@ SakuyaEngine (c) 2020-2021 Andrew Hong
 This code is licensed under MIT license (see LICENSE for details)
 """
 
+from typing import Tuple, List
+
 from .entity import Entity
 from .math import Vector
 
 class Bullet(Entity):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        position: Vector,
+        angle: float,
+        speed: float,
+        color: Tuple(int, int, int),
+        damage: float
+    ) -> None:
+        self.position = position
+        self.angle = angle
+        self.speed = speed
+        self.color = color
+        self.damage = damage
 
 class BulletSpawner:
-    def __init__(self, speed: float, frequency: float, starting_angle: float) -> None:
+    def __init__(
+        self,
+        starting_angle: float,
+        speed: float, 
+        frequency: float,
+        assigned_entity: Entity,
+        position_offset: Vector
+    ) -> None:
         """Constructor for BulletSpawner.
         
         This follows the Danmaku (弾幕) Theory.
 
         Parameters:
+            starting_angle: The spawner's starting angle in radians.
             speed: The bullet's default speed.
             frequency: The bullet's spawn rate frequency.
-            starting_angle: The spawner's starting angle in radians.
 
         """
-        pass
+        self.starting_angle = starting_angle
+        self.speed = speed
+        self.frequency = frequency
+        self.entity = assigned_entity
+        self.position_offset = position_offset
 
-    def spawn_bullet(self, acceleration: float) -> Bullet:
+    def spawn_bullet(self, acceleration: Vector) -> Bullet:
         pass
 
     def move_angle(self, new_angle: float, speed: float):
