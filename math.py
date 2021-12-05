2afabbf5161807df60a2493936bd3173f85f285a
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-05 01:11:10 -0500

    added support for line segment intersection

--- math.py
+++ math.py
@@ -2,7 +2,7 @@
 SakuyaEngine (c) 2020-2021 Andrew Hong
 This code is licensed under MIT license (see LICENSE for details)
 """
-from .errors import NegativeSpeedError
+from .errors import NegativeSpeedError, LineSegmentLinesError
 import math
 
 class Vector:
@@ -76,3 +76,44 @@ def move_toward(origin: float, target: float, speed: float):
 
     if target - origin < speed:
         return origin - speed
+
+def eval_segment_intersection(
+    point1: Vector, point2: Vector, 
+    point3: Vector, point4: Vector
+) -> Vector:
+    """Evaluates if 2 line segments collide with each other.
+
+    Parameters:
+        point1: The starting Vector of line 1.
+        point2: The ending Vector of line 1.
+        point3: The starting Vector of line 2.
+        point4: The ending Vector of line 2.
+    
+    Returns:
+        Line 1's intersecting Vector on line 2.
+
+    """
+    # NOTE: This function is a variant 
+    # from GameDen REWRITE for Novial's Gravity
+    x1, y1 = point1.x, point1.y
+    x2, y2 = point2.x, point2.y
+    x3, y3 = point3.x, point3.y
+    x4, y4 = point4.x, point4.y
+
+    dem = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+    if dem == 0:
+        raise LineSegmentLinesError
+
+    t1 = (x1-x3)*(y3-y4) - (y1-y3)*(x3-x4)
+    t = t1/dem
+    
+    u1 = (x1-x2)*(y1-y3) - (y1-y2)*(x1-x3)
+    u = -(u1/dem)
+
+    if t >= 0 and t <= 1 and u >= 0 and u <= 1:
+        return Vector(
+            x1 + t*(x2-x1),
+            y1 + t*(y2-y1)
+        )
+    else:
+        raise LineSegmentLinesError
