4097b3535519920a783e19d16349f700e0873d45
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-14 03:30:11 -0500

    Revived the old AI script

--- ai.py
+++ ai.py
@@ -2,11 +2,42 @@
 SakuyaEngine (c) 2020-2021 Andrew Hong
 This code is licensed under MIT license (see LICENSE for details)
 """
+import typing
+import random
+import pygame
 
-class Move:
-    def __init__(self):
-        pass
+class Decision:
+    def __init__(self, method, chance: float):
+        """
+        :param func method: the method that will be executed
+        :param float chance: the percentage of the decision happening
+        """
+        self.method = method
+        self.chance = chance
 
-class MoveSet:
-    def __init__(self):
-        pass
+class AI:
+    """
+    Makes a random decision every time an update_decisions() is executed
+    """
+    def __init__(self, decisions: typing.Sequence[Decision], update_tick, max_decisions: int=10):
+        self.decisions = decisions
+        self.max_decisions = max_decisions
+        self.used_ticks = []
+        self.update_tick = update_tick
+
+    def update_decisions(self, world_ticks_elapsed):
+        """
+        Returns the amount of decisions made
+        """
+        if pygame.time.get_ticks() not in self.used_ticks and world_ticks_elapsed % self.update_tick == 0:
+            decisions_made = 0
+            for d in self.decisions:
+                if decisions_made >= self.max_decisions: break
+
+                if d.chance > random.random():
+                    d.method()
+                    decisions_made += 1
+
+            self.used_ticks.append(pygame.time.get_ticks())
+            return decisions_made
+        return None
