17dc1ac1f53f7dae4a2794c845fbd957d049a324
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-12-20 10:48:35 -0500

    Cleanup

--- waves.py
+++ waves.py
@@ -5,10 +5,11 @@ This code is licensed under MIT license (see LICENSE for details)
 from random import randrange
 
 from .scene import Scene
-
 from .entity import Entity
 from .errors import NotEnoughArgumentsError, NotImplementedError
 from .events import EventSystem, WaitEvent
+
+import json
 
 class Sequence:
     def __init__(self):
@@ -62,6 +63,24 @@ class WaveManager:
     def load_wave(self, wave: int) -> None:
         raise NotImplementedError
     
+def _create_spawn_event(
+    wave_manager: WaveManager,
+    scene: Scene,
+    entity_key,
+    spawnpoint_key,
+    anim_key: int,
+    lifetime: int,
+    wait_time: int
+) -> WaitEvent:
+    def spawn_func(_entity_key, _spawnpoint_key, _anim_key, _lifetime, _event_system, _delta_time):
+        entity = wave_manager.spawn(_entity_key, _spawnpoint_key, _anim_key, _lifetime, _event_system, _delta_time)
+        scene.entities.append(entity)
+    spawn_event = WaitEvent("spawn_enemy", wait_time, spawn_func, args=[
+        entity_key, spawnpoint_key, anim_key, lifetime,
+        scene.event_system, scene.client.get_delta_time
+    ])
+    return spawn_event
+    
 def load_wave_file(path: str, wave_manager: WaveManager, scene: Scene) -> None:
     file = open(path, "r")
     wait_time = 0
@@ -70,15 +89,15 @@ def load_wave_file(path: str, wave_manager: WaveManager, scene: Scene) -> None:
         cmd = line.split(" ")
         if cmd[0] == "spawn":
             try:
-                def spawn_func(arg0, arg1, arg2, arg3, arg4, arg5):
-                    entity = wave_manager.spawn(arg0, arg1, arg2, arg3, arg4, arg5)
-                    scene.entities.append(entity)
-                spawn_event = WaitEvent("spawn_enemy", wait_time, spawn_func, args=[
-                    int(cmd[1]), int(cmd[2]), int(cmd[3]), int(cmd[4]),
-                    scene.event_system, scene.client.get_delta_time
-                ])
+                spawn_event = _create_spawn_event(wave_manager, scene, int(cmd[1]), int(cmd[2]), int(cmd[3]), int(cmd[4]), wait_time)
                 scene.event_system._methods.append(spawn_event)
             except IndexError:
                 raise NotEnoughArgumentsError
         if cmd[0] == "wait":
             wait_time += int(cmd[1])
+
+def load_wave_json(path: str, wave_manager: WaveManager, scene: Scene) -> None:
+    file = open(path, "r")
+    data = json.load(file)
+    
+    raise NotImplementedError
