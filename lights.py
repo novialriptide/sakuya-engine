6016f66d6d2e59e1bc7ce3a348c4c5a713f8f59f
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-23 23:53:49 -0500

    Added brightness setting to light()

--- lights.py
+++ lights.py
@@ -10,12 +10,13 @@ from .errors import NotImplementedError
 
 pygame_vector2 = TypeVar("pygame_vector2", Callable, pygame.Vector2)
 
-def light(surface: pygame.Surface, position: pygame_vector2, color: Tuple[int, int, int], radius: int):
-    circle_surf = pygame.Surface((radius*2, radius*2))
-    pygame.draw.circle(circle_surf, color, (radius, radius), radius)
-    circle_surf.set_colorkey((0, 0, 0))
+def light(surface: pygame.Surface, position: pygame_vector2, color: Tuple[int, int, int], radius: int, brightness: int = 1):
     position -= pygame.Vector2(radius, radius)
-    surface.blit(circle_surf, position, special_flags = pygame.BLEND_RGB_ADD)
+    for d in range(brightness):
+        circle_surf = pygame.Surface((radius*2, radius*2))
+        pygame.draw.circle(circle_surf, color, (radius, radius), radius * ( d / brightness))
+        circle_surf.set_colorkey((0, 0, 0))
+        surface.blit(circle_surf, position, special_flags = pygame.BLEND_RGB_ADD)
 
 def shadow(surface: pygame.Surface, position: pygame_vector2, darkness: int, radius: int):
     circle_surf = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
