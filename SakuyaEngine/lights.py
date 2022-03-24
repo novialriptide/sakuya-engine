50b772a268822de87158d0b7cd50741fe9b9a55d
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2022-03-24 18:12:09 -0400

    light optimization

--- SakuyaEngine/lights.py
+++ SakuyaEngine/lights.py
@@ -154,12 +154,12 @@ class LightRoom:
             angle2 = get_angle(position, line[1])
 
             point1 = (
-                int(length * 5 * math.cos(angle1)),
-                int(length * 5 * math.sin(angle1)),
+                int(length * 1.2 * math.cos(angle1)),
+                int(length * 1.2 * math.sin(angle1)),
             )
             point2 = (
-                int(length * 5 * math.cos(angle2)),
-                int(length * 5 * math.sin(angle2)),
+                int(length * 1.2 * math.cos(angle2)),
+                int(length * 1.2 * math.sin(angle2)),
             )
 
             points.append(pygame.Vector2(line[1]) + point2)
