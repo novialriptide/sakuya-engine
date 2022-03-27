8d980510133c2f717806fe8c8b66b58fd207b0c7
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-03-26 22:40:34 -0400

    renamed health to val

--- SakuyaEngine/bar.py
+++ SakuyaEngine/bar.py
@@ -6,23 +6,23 @@ from .math import move_toward
 
 
 class Bar:
-    def __init__(self, max_health, update_speed):
+    def __init__(self, max_val, update_speed, init_val: float = 0):
         """
         :param float max_health:
 
         In order for this to work properly, set the current_health
         value to whatever the health of the boss will be.
         """
-        self.max_health = max_health
-        self.current_health = max_health
-        self._display_health = max_health
+        self.max_val = max_val
+        self.current_val = init_val
+        self._display_val = init_val
         self.update_speed = update_speed
 
     @property
-    def display_health(self):
-        return self._display_health
+    def display_val(self):
+        return self._display_val
 
     def update(self, delta_time: float):
-        self._display_health = move_toward(
-            self._display_health, self.current_health, self.update_speed * delta_time
+        self._display_val = move_toward(
+            self._display_val, self.current_val, self.update_speed * delta_time
         )
