42906eca84cbee476cb13b5c2916f0ee6c3901ef
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-06-13 22:36:22 -0400

    Added volume modifier

--- SakuyaEngine/sounds.py
+++ SakuyaEngine/sounds.py
@@ -7,12 +7,24 @@ __all__ = ["Sound"]
 
 
 class Sound:
-    def __init__(self, source: str, cooldown: int = 0) -> None:
+    def __init__(self, source: str, cooldown: int = 0, volume: float = 1) -> None:
         self.source = source
         self._pg_source = pygame.mixer.Sound(self.source)
         self.cooldown = cooldown
         self._cooldown_clock = Clock(pause_upon_start=True)
         self._playing = False
+        self._volume = volume
+        self._pg_source.set_volume(self._volume)
+        self._volume_modifier = 1
+
+    @property
+    def volume_modifier(self) -> float:
+        return self._volume_modifier
+
+    @volume_modifier.setter
+    def volume_modifier(self, value: float) -> None:
+        self._volume_modifier = value
+        self._pg_source.set_volume(self._volume * self._volume_modifier)
 
     def play(self, repeat: bool = False) -> None:
         if repeat and self.cooldown == 0:
